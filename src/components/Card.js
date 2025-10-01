92488cdea7cc44569d2cfffa3552a02dbce419d6
Дмитрий <dimabogolepov2002@mail.ru>
2025-10-01 18:24:40 +0300

    Added Card.css for component. Fully complete styles and animation for cards opening or closing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import './Card.css'
 class Card extends React.Component {
 
 
     constructor() {
         super();
-        this.state = {isOpened:false};
+        this.state = {isOpened: false};
     }
+
     cardClickHandler(item) {
         this.props.onChoice(item)
     }
+
     render() {
         return (
-            <div className="card" onClick={this.cardClickHandler.bind(this, this.props.item)}>
-            <img src={'/images/'+ this.props.item.image} alt={this.props.item.name}/>
+            <div className={'card' + (this.state.isOpened ? ' opened' : ' closed')}
+                 onClick={this.cardClickHandler.bind(this, this.props.item)}>
+
+                <div className="card-inner card-front">
+                    <img src={'/images/' + this.props.item.image} alt={this.props.item.name}/>
+                </div>
+                <div className="card-inner card-back">
+                    <img src={'/images/question.svg'} alt="question mark"/>
+                </div>
             </div>
         )
     }
